f6f079bbff8f3e44cfc023ebf7a2bcce385360b3
Edith-E <69354983+Edith-E@users.noreply.github.com>
2023-04-10 17:58:30 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -214,28 +214,17 @@ function redCyclists(){
 //  distance = 0;
 // }
 
-//function reset(){
-//  gameState = PLAY;
-//  gameOver.visible = false;
-//  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
-  
-//  pinkCG.destroyEach();
-//  yellowCG.destroyEach();
-//  redCG.destroyEach();
-  
-//  distance = 0;
-// }
-
 function reset(){
-  gameState = END;
-  gameOver.visible = true;
-  mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
+ gameState = PLAY;
+ gameOver.visible = false;
+ mainCyclist.addAnimation("SahilRunning",mainRacerImg1);
   
-  pinkCG.destroyEach();
-  yellowCG.destroyEach();
-  redCG.destroyEach();
+ pinkCG.destroyEach();
+ yellowCG.destroyEach();
+ redCG.destroyEach();
   
-  distance = 50;
- }
+ distance = 0;
+}
+
 
 
